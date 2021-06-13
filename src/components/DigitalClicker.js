b72852bfadaf6fca32efb0bd0b65f0966028a7f2
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-13 15:18:47 -0500

    Completed Assignment

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+import React from 'react' 
+
+class DigitalClicker extends React.Component{
+    //has an initial state property timesClicked intially defined as 0 
+    //renders out a button with a label that shows timesClicked value, you button should just say 0 at the start 
+    //Whenever the button is clicked, update the state by incrementing the timesClicked by 1
+
+    constructor() {
+        super() 
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+
+    render() {
+        return(
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,71 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component {
+    //Should have an initial state with properties 
+    //create a button with the class 'birate' Clicking it changes the setting.birate state property to 12 
+    //create a button with the class 'resolution'. When clicked changes the settings.video resolution state property to '720p'
+    constructor() {
+        super() 
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    handleChangeBirate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+    
+
+    handleChangeResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+        })
+
+    }
+
+    /*
+   Or this can be defined using the Object.assign method:
+   
+   this.setState({
+     settings: Object.assign({}, this.state.settings, {
+       video: Object.assign({}, this.state.settings.video, {
+         resolution: '720p'
+       })
+     })
+   });
+   
+   */
+
+
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleChangeBirate}>Change bitrate</button>
+                <button className="resolution" onClick={this.handleChangeResolution}>Change resolution</button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
